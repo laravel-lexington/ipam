8c1d9e559a7ac65cca9629787118c103e46db766
Charles Combs <charlescombs@Charless-MacBook-Pro.local>
2016-08-19 20:12:19 -0400

    installs elixir

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,16 @@
 <?php
-
+//TODO clean up seeder file
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Users;
+use App\Sessions;
+use App\Payments;
+use App\PaymentMethods;
+use App\PageViews;
+use App\Orders;
+use App\Items;
+use App\Downloads;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +22,32 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        Users::truncate();
+        factory(Users::class, 50)->create();
+
+        Sessions::truncate();
+        factory(Sessions::class, 50)->create();
+
+        Payments::truncate();
+        factory(Payments::class, 50)->create();
+
+        PaymentMethods::truncate();
+        factory(PaymentMethods::class, 50)->create();
+
+        PageViews::truncate();
+        factory(PageViews::class, 50)->create();
+
+        Orders::truncate();
+        factory(Orders::class, 50)->create();
+
+        Items::truncate();
+        factory(Items::class, 50)->create();
+
+        Downloads::truncate();
+        factory(Downloads::class, 50)->create();
+
+        Model::reguard();
     }
 }

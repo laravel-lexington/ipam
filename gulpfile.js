74aad3489c1cb5ebae3f6c0d6ba31f4fae9b0d74
Charles Combs <charlescombs@Charless-MacBook-Pro.local>
2016-08-19 20:18:58 -0400

    adds gulpfile.js, assests files for dashboards

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,33 @@ var elixir = require('laravel-elixir');
  |
  */
 
-elixir(function(mix) {
-    mix.sass('app.scss');
+// elixir(function(mix) {
+//     mix.sass('app.scss');
+// });
+
+elixir(function(mix){
+
+    mix.styles('toolkit-inverse.css');
+    mix.styles('toolkit-light.css');
+    mix.styles('application.css');
+
+    //remember to place fonts in public/fonts and in public/build/fonts
+
+    mix.scripts('jquery.min.js');
+    mix.scripts('chart.js');
+    mix.scripts('tablesorter.min.js');
+    mix.scripts('toolkit.js');
+    mix.scripts('application.js');
+
+    mix.version([
+        'css/toolkit-inverse.css',
+        'css/toolkit-light.css',
+        'css/application.css',
+        'js/jquery.min.js',
+        'js/chart.js',
+        'js/tablesorter.min.js',
+        'js/toolkit.js',
+        'js/application.js'
+    ])
+
 });

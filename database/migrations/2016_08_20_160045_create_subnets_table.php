5f1501762ee830ef9bd8878657ce13b1eab5ccfd
Charles Combs <charlescombs@Charless-MacBook-Pro.local>
2016-08-20 12:51:12 -0400

    adds subnets class and table migration to repo

---
+++ database/migrations/2016_08_20_160045_create_subnets_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSubnetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('subnets', function (Blueprint $table) {
+
+            //mine
+            $table->increments('id'); //id of subnet pk
+
+            $table->integer('site_id'); //fk
+            //$table->foreign('site_id')->references('id')->on('sites')->onDelete('cascade'); //fk constraint
+
+            $table->integer('subnet_node_id'); //fk
+            //$table->foreign('subnet_node_id')->references('id')->on('subnet_nodes')->onDelete('cascade'); //fk constraint
+
+            $table->ipAddress('ip_address');
+            $table->smallInteger('prefix_length');
+            $table->string('name'); //??? name of what?
+            $table->ipAddress('default_gateway');
+
+            $table->timestamps();
+
+
+            //schema
+//            $table->increments('vlan_id'); //pk
+//
+//            $table->integer('site_id'); //fk
+//            $table->foreign('site_id')->references('...field...')->on('...table...'); //fk constraint
+//
+//            $table->integer('subnet_id'); //fk
+//            $table->foreign('subnet_id')->references('...field...')->on('...table...'); //fk constraint
+//
+//            $table->ipAddress('ip_address');
+//            $table->smallInteger('prefix_length');
+//            $table->string('name');
+//            $table->ipAddress('default_gateway');
+//
+//            $table->timestamps();
+//
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //DB::table('subnets')->dropForeign('subnet_node_id');
+        Schema::drop('subnets');
+    }
+}

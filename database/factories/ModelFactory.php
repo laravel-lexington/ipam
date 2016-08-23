f071a981bd7cf35628d7e3c42156274a54c46ca0
Charles Combs <charlescombs@Charless-MacBook-Pro.local>
2016-08-22 21:05:10 -0400

    adds app/Network class for IP, CIDR, SUBNET calculations, improves ModelFactory with app/Network

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,15 +11,6 @@
 |
 */
 
-//$factory->define(App\User::class, function (Faker\Generator $faker) {
-//    return [
-//        'name' => $faker->name,
-//        'email' => $faker->safeEmail,
-//        'password' => bcrypt(str_random(10)),
-//        'remember_token' => str_random(10)
-//    ];
-//});
-
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
@@ -34,31 +25,43 @@ $factory->define(App\Subnets::class, function (Faker\Generator $faker) {
         'site_id' => $faker->numberBetween(1, 50),
         'subnet_node_id' => $faker->numberBetween(1, 50),
         'ip_address' => function(){
-            return long2ip(rand(0, "4294967295"));
+            $fake_ip = long2ip(rand(0, "4294967295"));
+            return $fake_ip;
 
-//            $X3_1 = random_int(0,255);
-//            $X3_2 = random_int(0,255);
-//            $X3_3 = random_int(0,255);
-//            $X3_4 = random_int(0,255);
-//
-//            $fake_ip = $X3_1 . '.' . $X3_2 . '.' . $X3_3 . '.' . $X3_4;
-
-//            return $fake_ip;
         },
-        'prefix_length' => 24,
+
+        'prefix_length' => function(){
+            $fake_netmask = long2ip(rand(0, "4294967295"));
+            $masker = new \App\Network();
+            $prefix_length = $masker->netmask2cidr($fake_netmask);
+            return $prefix_length;
+        },
         'name' => $faker->name,
         'default_gateway' => function(){
+            //$ip = $this['ip_address']; //wrong reference
+            //$cidr = $this['prefix_length'];// wrong reference
 
-            return long2ip(rand(0, "4294967295"));
+            //same calculation method as 'ip_address' =>
+            //$ip = function(){
+                //$fake_ip = long2ip(rand(0, "4294967295"));
+                //return $fake_ip;
+                //return long2ip(rand(0, "4294967295"));
+            //};
 
-//            $X3_1 = random_int(0,255);
-//            $X3_2 = random_int(0,255);
-//            $X3_3 = random_int(0,255);
-//            $X3_4 = random_int(0,255);
-//
-//            $default_gate_number = $X3_1 . '.' . $X3_2 . '.' . $X3_3 . '.' . $X3_4;
-//
-//            return $default_gate_number;
+            $ip = long2ip(rand(0, "4294967295"));// used because there is an argument type error (expects a string, above gives object)
+
+            //same calculation method as 'prefix_length' =>
+            //$cidr = function() {
+
+            //get a Closure error unless I do it this way.
+                $fake_netmask = long2ip(rand(0, "4294967295"));
+                $masker = new \App\Network();
+                $cidr = $masker->netmask2cidr($fake_netmask);
+                //return $prefix_length;
+            //};
+            $gateKeeper = new \App\Network();
+            $default_gateway = $gateKeeper->cidr2network($ip, $cidr);
+            return $default_gateway;
         },
     ];
 });
